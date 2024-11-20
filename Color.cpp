ae3c0503c810c74829bc1387ab625bd11e4b25b5
Jawand Singh <jawandsingh@gmail.com>
2024-11-20 14:44:46 -0500

    create color fles

---
+++ Color.cpp
@@ -0,0 +1,37 @@
+#include "Color.hpp"
+
+using namespace std;
+
+// implement constructors
+Color::Color() {
+    this->r = 0;
+    this->g = 0;
+    this->b = 0;
+}
+Color::Color(double r, double g, double b) {
+    this->r = r;
+    this->g = g;
+    this->b = b;
+}
+
+// implement add/operator+ method
+Color Color::operator+(Color &c) {
+    return Color(this->r + c.r, this->g + c.g, this->b + c.b);
+}
+Color Color::operator+(double scalar) {
+    return Color(this->r + scalar, this->g + scalar, this->b + scalar);
+}
+// implement operator* method
+Color Color::operator*(double scalar) {
+    return Color(this->r * scalar, this->g * scalar, this->b * scalar);
+}
+
+// implement cap
+void Color::cap() {
+    if (this->r > 1) 
+        this->r = 1;
+    if (this->g > 1) 
+        this->g = 1;
+    if (this->b > 1) 
+        this->b = 1;
+}
---
+++ Color.hpp
@@ -0,0 +1,30 @@
+#ifndef COLOR_HPP
+#define COLOR_HPP
+
+using namespace std;
+
+class Color {
+    private:
+        double r, g, b;
+
+    public:
+        // constructors
+        Color() {}
+        Color(double r, double g, double b) {}
+
+        // getters 
+        inline double get_r() { return r; }
+        inline double get_g() { return g; }
+        inline double get_b() { return b; }
+
+        // add/operator+ method
+        Color operator+(Color &c) {}
+        Color operator+(double scalar) {}
+        // mult_scalar/operator* method
+        Color operator*(double scalar) {}
+
+        // cap method
+        void cap() {}
+};
+
+#endif
